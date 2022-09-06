b69990a3176ca1d37f414cecf290bb2e93901171
gequalspisquared <nickcrusher30@gmail.com>
2022-09-05 21:52:22 -0400

    starting js!

---
+++ js-basics/js_basics.js
@@ -0,0 +1,14 @@
+// Testing!
+console.log("Hello, World!");
+
+// Tasks at https://javascript.info/variables
+// Working with variables
+let user = "John";
+let admin = user;
+alert(admin);
+
+// Giving the right name
+let ourPlanetName = "Earth";
+let currentVisitor = "John";
+
+// Done
